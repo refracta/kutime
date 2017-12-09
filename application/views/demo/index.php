3780847dc55b10da2a567f4f3d8f5ae56e1a6870
Hyeonjong <nianelo4@gmail.com>
2017-12-10 02:01:22 +0900

    Add Bulma 0.6.1

--- application/views/demo/index.php
+++ application/views/demo/index.php
@@ -6,6 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no">
 	<title>인하대 시간표</title>
 	<link rel="stylesheet" href="/assets/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="/assets/bulma-0.6.1/css/bulma.css">
 </head>
 <body>
 	<span>인하대 시간표 데모 페이지</span>
