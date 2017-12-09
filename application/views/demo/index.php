6923dc136606e8281e4231c26fe1d217d0c50c84
Hyeonjong <nianelo4@gmail.com>
2017-12-10 02:00:02 +0900

    Add Font Awesome 4.7.0

--- application/views/demo/index.php
+++ application/views/demo/index.php
@@ -5,6 +5,7 @@
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no">
 	<title>인하대 시간표</title>
+	<link rel="stylesheet" href="/assets/font-awesome-4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 	<span>인하대 시간표 데모 페이지</span>
