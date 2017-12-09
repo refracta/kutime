121f8238ef92cffffd5501f27d4a2bad7f170ba2
Hyeonjong <nianelo4@gmail.com>
2017-12-09 23:04:42 +0900

    Add demo page

---
+++ application/views/demo/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no">
+	<title>인하대 시간표</title>
+</head>
+<body>
+	<span>인하대 시간표 데모 페이지</span>
+</body>
+</html>
