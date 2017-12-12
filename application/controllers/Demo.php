a08d05cde0798899143ec6616ccb41af4bbe6c33
Hyeonjong <nianelo4@gmail.com>
2017-12-13 00:51:12 +0900

    Develop demo page

--- application/controllers/Demo.php
+++ application/controllers/Demo.php
@@ -5,6 +5,14 @@ class Demo extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('demo/index');
+		$intime_data = json_decode(file_get_contents('/data/intime/exported.json'), TRUE);
+
+		$data = array(
+			'lecture_list' => $intime_data['lecture']['list'],
+			'dept_name' => $intime_data['department'][0]['name'],
+			'lect_code_list' => $intime_data['department'][0]['lect_code_list'],
+		);
+
+		$this->load->view('demo/index', $data);
 	}
 }
--- application/views/demo/index.php
+++ application/views/demo/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="ko">
+<html lang="ko" class="has-navbar-fixed-top has-navbar-fixed-bottom">
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
@@ -7,8 +7,101 @@
 	<title>인하대 시간표</title>
 	<link rel="stylesheet" href="/assets/font-awesome-4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="/assets/bulma-0.6.1/css/bulma.css">
+	<link rel="stylesheet" href="/assets/custom/style.css">
 </head>
 <body>
-	<span>인하대 시간표 데모 페이지</span>
+	<section class="hero is-fullheight">
+		<main class="hero-body is-vertical-paddingless">
+			<div class="container">
+				<ul>
+<?php foreach ($lect_code_list as $code): ?>
+					<li class="box">
+						<article class="media">
+							<div class="media-content">
+								<div class="media-content">
+									<div class="content is-size-7-mobile">
+										<p>
+											<strong><?php echo $lecture_list[$code][2]; ?></strong> <small><?php echo $lecture_list[$code][0]; ?></small>
+											<br>
+											<small><?php echo $lecture_list[$code][6]; ?></small>
+										</p>
+									</div>
+								</div>
+							</div>
+							<div class="media-right">
+								<a class="button is-white">
+									<span class="icon is-medium">
+										<i class="fa fa-star-o fa-lg"></i>
+									</span>
+								</a>
+								<a class="button is-white">
+									<span class="icon is-medium">
+										<i class="fa fa-ellipsis-h fa-lg"></i>
+									</span>
+								</a>
+							</div>
+						</article>
+					</li>
+<?php endforeach; ?>
+				</ul>
+			</div>
+		</main>
+		<nav class="navbar is-fixed-top box is-paddingless is-radiusless" role="navigation">
+			<div class="container">
+				<div class="level is-mobile">
+					<div class="level-item is-marginless columns is-mobile is-gapless">
+						<div class="column">
+							<div class="box is-shadowless is-vertical-paddingless">
+								<small><?php echo $dept_name; ?></small>
+							</div>
+						</div>
+						<div class="column is-one-quarter has-text-centered">
+							<a class="button is-fullwidth is-white">
+								<span class="icon is-medium">
+									<i class="fa fa-filter fa-lg"></i>
+								</span>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+		<nav class="navbar is-fixed-bottom box is-paddingless is-radiusless" role="navigation">
+			<div class="container">
+				<div class="level is-mobile">
+					<div class="level-item is-marginless columns is-mobile is-gapless">
+						<div class="column has-text-centered">
+							<a class="button is-fullwidth is-link is-inverted">
+								<span class="icon is-medium">
+									<i class="fa fa-home fa-lg"></i>
+								</span>
+							</a>
+						</div>
+						<div class="column has-text-centered">
+							<a class="button is-fullwidth is-primary is-inverted">
+								<span class="icon is-medium">
+									<i class="fa fa-calendar fa-lg"></i>
+								</span>
+							</a>
+						</div>
+						<div class="column has-text-centered">
+							<a class="button is-fullwidth is-link is-inverted">
+								<span class="icon is-medium">
+									<i class="fa fa-calculator fa-lg"></i>
+								</span>
+							</a>
+						</div>
+						<div class="column has-text-centered">
+							<a class="button is-fullwidth is-link is-inverted" disabled>
+								<span class="icon is-medium">
+									<i class="fa fa-user fa-lg"></i>
+								</span>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+	</section>
 </body>
 </html>
