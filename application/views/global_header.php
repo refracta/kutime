27c42b356523db6f9cb66a327846ee57b5e828db
Hyeonjong <nianelo4@gmail.com>
2017-12-17 21:45:06 +0900

    Add Lodash 4.17.4

--- application/views/global_header.php
+++ application/views/global_header.php
@@ -8,6 +8,7 @@
 	<link rel="stylesheet" href="/assets/font-awesome-4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="/assets/bulma-0.6.1/css/bulma.css">
 	<link rel="stylesheet" href="/assets/custom/style.css">
+	<script src="/assets/lodash-4.17.4/lodash.min.js"></script>
 <?php if (ENVIRONMENT === 'production'): ?>
 	<script src="/assets/vue-2.5.10/vue.min.js"></script>
 <?php else: ?>
