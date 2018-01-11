c8c2218bdb17523d6879e08d856c03a41d896623
Hyeonjong <nianelo4@gmail.com>
2018-01-12 00:10:46 +0900

    Install babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,19 @@ module.exports = {
 	entry: './application/assets/custom/index.js',
 	output: {
 		filename: './application/assets/custom/bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['env']
+					}
+				}
+			}
+		]
 	}
 };
