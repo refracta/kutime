71ffa24fd561747b4699978601b01411c2efaff1
Hyeonjong <nianelo4@gmail.com>
2018-01-16 23:21:05 +0900

    Change whitespace

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
 module.exports = {
-	entry: './application/assets/custom/index.js',
-	output: {
-		filename: './application/assets/custom/bundle.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['env']
-					}
-				}
-			}
-		]
-	}
+  entry: './application/assets/custom/index.js',
+  output: {
+    filename: './application/assets/custom/bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  }
 };
