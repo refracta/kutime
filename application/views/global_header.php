3f1c91a9dc585c12c80fc79eafdba26059c9df2c
Hyeonjong <nianelo4@gmail.com>
2018-01-21 15:16:46 +0900

    Delete files which added by old-fashioned

--- application/views/global_header.php
+++ application/views/global_header.php
@@ -5,16 +5,7 @@
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no">
 	<title>인하대 시간표</title>
-	<link rel="stylesheet" href="/assets/font-awesome-4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="/assets/bulma-0.6.1/css/bulma.css">
 	<link rel="stylesheet" href="/assets/custom/style.css">
-	<script src="/assets/lodash-4.17.4/lodash.min.js"></script>
-	<script src="/assets/axios-0.17.1/axios.min.js"></script>
-<?php if (ENVIRONMENT === 'production'): ?>
-	<script src="/assets/vue-2.5.10/vue.min.js"></script>
-<?php else: ?>
-	<script src="/assets/vue-2.5.10/vue.js"></script>
-<?php endif; ?>
 </head>
 <body>
 	<section id="intime-app" class="hero is-fullheight">
