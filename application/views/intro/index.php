5e5be9ef478d02a64d91b6d80a675c6e9830d0c7
Hyeonjong <nianelo4@gmail.com>
2018-02-04 01:14:46 +0900

    Use Sass with scss syntax

--- application/views/intro/index.php
+++ application/views/intro/index.php
@@ -1,4 +1,4 @@
 <?php require_once(VIEWPATH . 'global_header.php'); ?>
 	<section id="intime-app"></section>
-	<script src="/assets/intro.0e2df4305fa5a1cbb84f.js"></script>
+	<script src="/assets/intro.c2f6fc661d6b6312d32c.js"></script>
 <?php require_once(VIEWPATH . 'global_footer.php'); ?>
