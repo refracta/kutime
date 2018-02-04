cd348c937111cb1c768f0cef464b789f1dceed73
Hyeonjong <nianelo4@gmail.com>
2018-02-04 15:20:53 +0900

    Branch processing for css and js

--- application/views/intro/index.php
+++ application/views/intro/index.php
@@ -1,5 +1,10 @@
 <?php require_once(VIEWPATH . 'global_header.php'); ?>
 	<section id="intime-app"></section>
+<?php if (ENVIRONMENT === 'production'): ?>
 	<script src="/assets/vendor.c93e6221e63230f6739f.js"></script>
 	<script src="/assets/intro.b3d22bae0a0f8c979fab.js"></script>
+<?php else: ?>
+	<script src="/dev-assets/vendor.js"></script>
+	<script src="/dev-assets/intro.bundle.js"></script>
+<?php endif; ?>
 <?php require_once(VIEWPATH . 'global_footer.php'); ?>
