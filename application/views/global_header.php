a4262b10628cda8c548718edfd28720f763b11cd
Hyeonjong <nianelo4@gmail.com>
2018-02-04 14:32:07 +0900

    Divide css and js

--- application/views/global_header.php
+++ application/views/global_header.php
@@ -5,5 +5,6 @@
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no">
 	<title>인하대 시간표</title>
+	<link rel="stylesheet" href="/assets/global.b2476478ba2d5f4b81db.css">
 </head>
 <body>
--- application/views/intro/index.php
+++ application/views/intro/index.php
@@ -1,4 +1,5 @@
 <?php require_once(VIEWPATH . 'global_header.php'); ?>
 	<section id="intime-app"></section>
-	<script src="/assets/intro.c2f6fc661d6b6312d32c.js"></script>
+	<script src="/assets/vendor.c93e6221e63230f6739f.js"></script>
+	<script src="/assets/intro.b3d22bae0a0f8c979fab.js"></script>
 <?php require_once(VIEWPATH . 'global_footer.php'); ?>
