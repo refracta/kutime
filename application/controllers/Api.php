cd44d15aeb0b8b2f3b1689e99b9fa4a3e834e4bf
Hyeonjong <nianelo4@gmail.com>
2018-02-12 14:45:29 +0900

    Merge apis which get department list

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -13,7 +13,7 @@ class Api extends CI_Controller {
 		$this->output->set_content_type('application/json', 'utf-8');
 	}
 
-	public function departments()
+	public function categories()
 	{
 		$allowed_methods = array('GET');
 
@@ -23,6 +23,8 @@ class Api extends CI_Controller {
 		{
 			show_error('Request method is not supported.', 405, '405 Method Not Allowed');
 		}
+
+		$response_data = array();
 
 		foreach ($this->intime_data['department'] as &$data)
 		{
@@ -30,27 +32,16 @@ class Api extends CI_Controller {
 		}
 		unset($data);
 
-		echo json_encode($this->intime_data['department']);
-	}
-
-	public function others()
-	{
-		$allowed_methods = array('GET');
-
-		header('Access-Control-Allow-Methods: ' . implode(', ', $allowed_methods));
-
-		if ( ! in_array($this->input->server('REQUEST_METHOD'), $allowed_methods))
-		{
-			show_error('Request method is not supported.', 405, '405 Method Not Allowed');
-		}
-
 		foreach ($this->intime_data['additional'] as &$data)
 		{
 			unset($data['lect_code_list']);
 		}
 		unset($data);
 
-		echo json_encode($this->intime_data['additional']);
+		$response_data['departments'] = $this->intime_data['department'];
+		$response_data['others'] = $this->intime_data['additional'];
+
+		echo json_encode($response_data);
 	}
 
 	public function lectures($code)
