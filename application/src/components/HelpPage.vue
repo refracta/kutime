3bc0fae143ad9f8e80d05ba4bb40db468786f2d8
Hyeonjong <nianelo4@gmail.com>
2018-02-18 00:36:41 +0900

    Add help page component

---
+++ application/src/components/HelpPage.vue
@@ -0,0 +1,28 @@
+<template>
+	<section class="hero is-fullheight">
+		<main class="hero-body is-vertical-paddingless">
+			<div class="container">
+				<span>도움말 페이지</span>
+			</div>
+		</main>
+
+		<TitleBar/>
+
+		<MenuBar/>
+	</section>
+</template>
+
+<script>
+import TitleBar from './TitleBar.vue';
+import MenuBar from './MenuBar.vue';
+
+export default {
+	name: 'helpPage',
+	components: {
+		TitleBar, MenuBar
+	},
+	data () {
+		return {};
+	}
+};
+</script>
