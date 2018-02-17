83a8e9b697aa3ac6b8fb1d2f467051b6f187c7ad
Hyeonjong <nianelo4@gmail.com>
2018-02-18 02:28:52 +0900

    Update help page

--- application/src/components/HelpPage.vue
+++ application/src/components/HelpPage.vue
@@ -2,7 +2,21 @@
 	<section class="hero is-fullheight">
 		<main class="hero-body is-vertical-paddingless">
 			<div class="container">
-				<span>도움말 페이지</span>
+				<p class="box has-text-centered is-shadowless">
+					<span>인하대 시간표 버전 - 0.4.0</span>
+				</p>
+
+				<p class="box has-text-centered">
+					<a class="is-block" href="http://www.facebook.com/InhaTimetable" target="_blank">
+						<span class="icon"><i class="fa fa-facebook-official fa-lg"></i></span><span>문의사항 / 버그제보</span>
+					</a>
+				</p>
+
+				<p class="level has-text-centered">
+					<a href="https://bulma.io" target="_blank">
+						<img src="../assets/made-with-bulma.png" alt="Made with Bulma" width="256" height="48">
+					</a>
+				</p>
 			</div>
 		</main>
 
