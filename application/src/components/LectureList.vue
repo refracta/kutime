ea99366526eb75d743d97cb1fab24f7a6a509ec2
Hyeonjong <nianelo4@gmail.com>
2018-02-24 02:48:32 +0900

    Implement function which toggle lecture for favorites

--- application/src/components/LectureList.vue
+++ application/src/components/LectureList.vue
@@ -16,9 +16,12 @@
 					</div>
 				</div>
 				<div class="media-right">
-					<a class="button is-white">
+					<a class="button"
+						:class="[lecture[14].isStarred ? 'is-starred' : 'is-white']"
+						:disabled="!canUseStorage"
+						@click="[lecture[14].isStarred ? removeLecture(index) : addLecture(index)]">
 						<span class="icon is-medium">
-							<i class="fa fa-star-o fa-lg"></i>
+							<i class="fa fa-lg" :class="[lecture[14].isStarred ? 'fa-star' : 'fa-star-o']"></i>
 						</span>
 					</a>
 					<a class="button is-white" @click="openDetail(index)">
@@ -38,7 +41,8 @@
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { mapState, mapGetters } from 'vuex';
+import { storage } from '../utils/web-storage';
 
 export default {
 	name: 'lectureList',
@@ -46,19 +50,51 @@ export default {
 		return {};
 	},
 	computed: {
+		canUseStorage () {
+			return (storage !== null);
+		},
 		isGuiding () {
 			return (this.activatedCode === '' || this.isLoading);
 		},
 		...mapState([
-			'lectureList',
 			'activatedCode',
 			'isLoading',
 			'isReady',
+		]),
+		...mapState({
+			lectureList (state) {
+				let originalList = state.lectureList;
+				let newList = [];
+
+				for (let idx = 0; idx < originalList.length; idx += 1) {
+					let lecture = originalList[idx].slice();
+
+					lecture.push({
+						isStarred: (this.starredList.indexOf(lecture[0]) !== -1),
+					});
+					newList.push(lecture);
+				}
+
+				return newList;
+			},
+		}),
+		...mapGetters([
+			'starredList',
 		])
 	},
 	methods: {
 		openDetail (idx) {
 			this.$store.commit('openDetail', idx);
+		},
+		addLecture (idx) {
+			if (this.canUseStorage) {
+				this.$store.commit('addLecture', idx);
+			}
+		},
+		removeLecture (idx) {
+			if (this.canUseStorage) {
+				this.$store.commit('removeLecture', idx);
+			}
 		}
 	}
 };
--- application/src/lecture.index.js
+++ application/src/lecture.index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import LecturePage from './components/LecturePage.vue';
+import { storage } from './utils/web-storage';
 import 'bulma/css/bulma.css';
 import 'font-awesome/scss/font-awesome.scss';
 import './assets/global-tuning.scss';
@@ -15,10 +16,16 @@ const store = new Vuex.Store({
 		activatedCode: '',
 		activatedIndex: null,
 		activatedName: '',
+		starredCodes: storage.getItem('starredCodes'),
 		isDetailing: false,
 		isFiltering: false,
 		isLoading: null,
 		isReady: false
+	},
+	getters: {
+		starredList (state) {
+			return JSON.parse(state.starredCodes);
+		},
 	},
 	mutations: {
 		readyForLectures (state, payload) {
@@ -34,6 +41,26 @@ const store = new Vuex.Store({
 			state.lectureList = payload.lectures.slice();
 			state.activatedName = payload.name;
 			state.isLoading = false;
+		},
+		addLecture (state, payload) {
+			let lectureCode = state.lectureList[payload][0];
+			let starredList = JSON.parse(state.starredCodes);
+
+			if (starredList.indexOf(lectureCode) === -1) {
+				state.starredCodes = JSON.stringify([...starredList, lectureCode]);
+				storage.setItem('starredCodes', state.starredCodes);
+			}
+		},
+		removeLecture (state, payload) {
+			let lectureCode = state.lectureList[payload][0];
+			let starredList = JSON.parse(state.starredCodes);
+			let idx = starredList.indexOf(lectureCode);
+
+			if (idx !== -1) {
+				starredList.splice(idx, 1);
+				state.starredCodes = JSON.stringify(starredList);
+				storage.setItem('starredCodes', state.starredCodes);
+			}
 		},
 		openDetail (state, payload) {
 			state.activatedIndex = payload;
