904249ad8839dc64289d64e6a2182a736c4c3d70
Hyeonjong <nianelo4@gmail.com>
2018-03-08 23:05:17 +0900

    Update eslint config (whitespace rule)

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,8 @@ module.exports = {
   },
   // add your custom rules here
   rules: {
+    indent: ['error', 'tab'],
+    'no-tabs': 'off',
     // don't require .vue extension when importing
     'import/extensions': ['error', 'always', {
       js: 'never',
