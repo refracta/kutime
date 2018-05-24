4736ae34fd844d8641da761904fc7a9b6c30e049
Hyeonjong <nianelo4@gmail.com>
2018-05-24 23:19:59 +0900

    change style import syntax

--- application/src/intro.index.js
+++ application/src/intro.index.js
@@ -1,6 +1,4 @@
 import Vue from 'vue';
-import 'bulma/css/bulma.css';
-import 'bulma-extensions/bulma-tooltip/dist/bulma-tooltip.min.css';
 import 'font-awesome/scss/font-awesome.scss';
 import IntroPage from './components/IntroPage';
 import './assets/global-tuning.scss';
