7f4db799744961d8ffba6fdc4ce4f18b02b504f8
Hyeonjong <nianelo4@gmail.com>
2019-07-08 14:53:17 +0900

    Define existing api as version 0 api

--- api/index.js
+++ api/index.js
@@ -5,9 +5,11 @@ const app = express()
 
 // Require API routes
 const users = require('./routes/users')
+const version0 = require('./routes/v0')
 
 // Import API Routes
 app.use(users)
+app.use(version0)
 
 // Export the server middleware
 module.exports = {
