7ff8103c86ae6b6bf36a47d7db42164a0f9984f3
Hyeonjong <nianelo4@gmail.com>
2019-07-10 23:50:42 +0900

    Enumerate course schedule

--- pages/course-schedule.vue
+++ pages/course-schedule.vue
@@ -23,6 +23,41 @@
         @click="getCourses"
       >조회</v-btn>
     </v-flex>
+    <v-flex class="mt-3">
+      <v-card>
+        <v-card-title>
+          <v-spacer></v-spacer>
+          <v-text-field
+            v-model="keyword"
+            append-icon="fas fa-search"
+            label="결과 내 검색"
+            single-line
+            hide-details
+          ></v-text-field>
+        </v-card-title>
+        <v-data-table
+          disable-initial-sort
+          :headers="headers"
+          :items="courses"
+          item-key="id"
+          :search="keyword"
+          :rows-per-page-items="rowsPerPage"
+        >
+          <template v-slot:items="props">
+            <td>{{ props.item.id }}</td>
+            <td>{{ props.item.classGroup }}</td>
+            <td>{{ props.item.name }}</td>
+            <td>{{ props.item.grade }}</td>
+            <td>{{ props.item.credit }}</td>
+            <td>{{ props.item.type }}</td>
+            <td>{{ props.item.timePlace }}</td>
+            <td>{{ props.item.professor }}</td>
+            <td>{{ props.item.evaluation }}</td>
+            <td>{{ props.item.note }}</td>
+          </template>
+        </v-data-table>
+      </v-card>
+    </v-flex>
   </v-layout>
 </template>
 
@@ -83,6 +118,21 @@ export default {
       selectedCategoryId: '',
       majorCategories: [],
       otherCategories: [],
+      headers: [
+        { text: '학수번호', value: 'id', width: '140' },
+        { text: '분반그룹', value: 'classGroup', width: '140' },
+        { text: '과목명', value: 'name', width: '290' },
+        { text: '학년', value: 'grade', width: '90' },
+        { text: '학점', value: 'credit', width: '90' },
+        { text: '과목구분', value: 'type', width: '115' },
+        { text: '시간 및 강의실', value: 'timePlace', width: '290' },
+        { text: '담당교수', value: 'professor', width: '140' },
+        { text: '평가방식', value: 'evaluation', width: '115' },
+        { text: '비고', value: 'note', width: '190' }
+      ],
+      courses: [],
+      keyword: '',
+      rowsPerPage: [10, 20, 30],
       isLoadingCourses: false
     }
   },
@@ -105,12 +155,14 @@ export default {
       }`
       return axios.post('/graphql', { query })
         .then((res) => {
-          console.log(res.data)
+          const { courses } = res.data.data
+          this.courses = courses
         })
         .catch((e) => {
           console.error(e)
         })
         .finally(() => {
+          this.keyword = ''
           this.isLoadingCourses = false
         })
     }
