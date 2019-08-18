367723a4265d8b05cedcd57ecd3a822fa8a6a06b
Hyeonjong <nianelo4@gmail.com>
2019-08-19 01:08:45 +0900

    Remove unused API

--- api/index.js
+++ api/index.js
@@ -4,11 +4,9 @@ const express = require('express')
 const app = express()
 
 // Require API routes
-const users = require('./routes/users')
 const version0 = require('./routes/v0')
 
 // Import API Routes
-app.use(users)
 app.use(version0)
 
 // Export the server middleware
