5fa7b5605ec728bb4c43539d1a411a8435a63032
refracta <58779799+refracta@users.noreply.github.com>
2024-01-02 05:49:30 +0900

    Minor fix

--- scripts/run-server.sh
+++ scripts/run-server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-tmux new -d -s server
 tmux kill-session -t server 2> /dev/null
+tmux new -d -s server
 tmux send-keys -t server "bash" C-m
 tmux send-keys -t server "cd /usr/src" C-m
 tmux send-keys -t server "yarn run start" C-m
